20263c83d351a47869fb757d39d9e80078f978e9
chaitalichaudhari29 <165438714+chaitalichaudhari29@users.noreply.github.com>
2026-01-21 21:30:36 +0530

    Updated LC_1672_Richest_customer_wealth.java

--- arrays/LC_1672_Richest_customer_wealth.java
+++ arrays/LC_1672_Richest_customer_wealth.java
@@ -1,3 +1,22 @@
+/**
+ * LeetCode 1672 - Richest Customer Wealth
+ *
+ * Difficulty: Easy
+ *
+ * Problem:
+ * Given a 2D array where accounts[i][j] represents the amount of money
+ * the i-th customer has in the j-th bank.
+ *
+ * Approach:
+ * - Traverse each customer's accounts
+ * - Compute total wealth per customer
+ * - Track maximum wealth
+ *
+ * Time Complexity: O(m * n)
+ * Space Complexity: O(1)
+ *
+ * Pattern: Array Traversal
+ */
 class Solution {
     public int maximumWealth(int[][] accounts) {
      	int maxWealth = 0;
