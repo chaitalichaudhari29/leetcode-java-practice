16845b12480fd3954aca43b8094e0a05589daac7
chaitalichaudhari29 <165438714+chaitalichaudhari29@users.noreply.github.com>
2026-01-21 21:31:33 +0530

    Updated LC_1480_Running_sum_1D_array.java

--- arrays/LC_1480_Running_sum_1D_array.java
+++ arrays/LC_1480_Running_sum_1D_array.java
@@ -1,3 +1,27 @@
+/**
+ * LeetCode 1480 - Running Sum of 1D Array
+ *
+ * Difficulty: Easy
+ *
+ * Problem:
+ * Given an array nums, return the running sum of nums.
+ * The running sum at index i is the sum of all elements
+ * from index 0 to i (inclusive).
+ *
+ * Example:
+ * Input: nums = [1,2,3,4]
+ * Output: [1,3,6,10]
+ *
+ * Approach:
+ * - Iterate from index 1 onwards
+ * - Add the previous element's sum to the current element
+ * - Modify the array in-place to save space
+ *
+ * Time Complexity: O(n)
+ * Space Complexity: O(1)
+ *
+ * Pattern: Prefix Sum / Running Sum
+ */
 class Solution {
     public int[] runningSum(int[] nums) {
         int[] runningSum = new int[nums.length];
